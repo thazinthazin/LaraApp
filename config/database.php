e4ae92a87b9e717e5a3174f4bfd48baa2265a67e
thazin thazin <thazinhtun042017@gmail.com>
2018-07-24 22:14:50 +0630

    create login and register

--- config/database.php
+++ config/database.php
@@ -53,16 +53,20 @@ return array(
                 ),
 
 
-                'mysql' => array(
-                        'driver'    => 'mysql',
-                        'host'      => 'localhost',
-                        'database'  => 'jhntfgnzax',
-                        'username'  => 'jhntfgnzax',
-                        'password'  => 'gF3uNG7Ydz',
-                        'charset'   => 'utf8',
-                        'collation' => 'utf8_unicode_ci',
-                        'prefix'    => '',
-                ),
+                'mysql' => [
+                        'driver' => 'mysql',
+                        'host' => env('DB_HOST', '127.0.0.1'),
+                        'port' => env('DB_PORT', '3306'),
+                        'database' => env('DB_DATABASE', 'forge'),
+                        'username' => env('DB_USERNAME', 'forge'),
+                        'password' => env('DB_PASSWORD', ''),
+                        'unix_socket' => env('DB_SOCKET', ''),
+                        'charset' => 'utf8mb4',
+                        'collation' => 'utf8mb4_unicode_ci',
+                        'prefix' => '',
+                        'strict' => true,
+                        'engine' => null,
+                    ],
 
                 'pgsql' => array(
                         'driver'   => 'pgsql',
