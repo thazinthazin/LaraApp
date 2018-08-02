ecad080397a85ee765fb63069ee94bd3931bcffe
thazin thazin <thazinhtun042017@gmail.com>
2018-08-02 10:12:04 +0630

    Socialite login update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\SocialProvider;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Redirect;
 
 class RegisterController extends Controller
 {
@@ -67,5 +70,46 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        try {
+            $socialUser = Socialite::driver($provider)->user();
+        } catch (\Exception $e) {
+            return Redirect::to('/');
+        }
+
+        $socialProvider = SocialProvider::where('provider_id',$socialUser->getid())->first();
+
+        if (!$socialProvider){
+            $user = User::firstOrCreate(
+                ['email' => $socialUser->getEmail],
+                ['name' => $socialUser->getName()]
+            );
+            $user->socialProviders()->create(
+                ['provider_id' => $socialUser->getId, 'provider' => $provider]
+            );
+        } else
+            $user = $socialProvider->user;
+
+        auth()->login($user);
+
+        return redirect('/home');
     }
 }
